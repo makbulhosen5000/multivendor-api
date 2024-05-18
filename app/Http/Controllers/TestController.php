bcab928cf7a52e69cb20b7c1f8cbed59e5eb4055
makbulhosen5000 <makbulhosen5000@gmail.com>
2024-05-18 13:18:40 +0600

    fit:added spatie and passport authentication

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+     /**
+     * @OA\Get(
+     *    path="/api/test",
+     *     tags={"Ping"},
+     *     summary="Ping API",
+     *     description="Ping API",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean", example=true),
+     *             @OA\Property(property="message", type="string", example="Welcome to Ecommerce Multi Vendor API"),
+     *             @OA\Property(property="data", type="object", example="null"),
+     *         )
+     *     )
+     * )
+     */
+    public function index(){
+        return response()->json([
+            "success" => true,
+            "message" => "Welcome to multi vendor api",
+            'data' => null
+        ],200);
+    }
+}
